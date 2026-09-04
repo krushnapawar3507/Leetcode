8490df66261a38f600e77718ffc13633819a25fb
krushnapawar3507 <krushnapawar3507@gmail.com>
2026-09-04 18:33:57 +0530

    Time: 2806 ms (5.01%), Space: 0B (100%) - LeetHub

---
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select Person.firstName , Person.lastname , Address.city ,Address.state from Person left join Address on person.personId = Address.personId;
