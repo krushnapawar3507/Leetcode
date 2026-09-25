0510b2d79937216094e01cebdaa0093e6dd9758e
krushnapawar3507 <krushnapawar3507@gmail.com>
2026-09-25 17:42:13 +0530

    Time: 1 ms (77.69%), Space: 47 MB (9.18%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,21 +1,34 @@
+// class Solution {
+//     public int removeDuplicates(int[] nums) {
+//         if(nums.length == 0){
+//             return 0;
+//         }
+//         int i=0;
+//         int j = 1;
+//         while(j<nums.length){
+//             if(nums[i] == nums[j]){
+//                 j++;
+//             }
+//             else{
+//                 nums[i+1] = nums[j];
+//                 i++;
+//                 j++;
+//             }
+            
+//         }
+//         return i+1;
+//     }
+// }
 class Solution {
     public int removeDuplicates(int[] nums) {
-        if(nums.length == 0){
-            return 0;
-        }
-        int i=0;
-        int j = 1;
-        while(j<nums.length){
-            if(nums[i] == nums[j]){
-                j++;
+        
+        int count = 1;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i] != nums[i+1]){
+                nums[count]= nums[i+1];
+                count++;
             }
-            else{
-                nums[i+1] = nums[j];
-                i++;
-                j++;
-            }
-            
         }
-        return i+1;
+        return count;
     }
 }
